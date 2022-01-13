f4f88ae7c297296f2cf9f9c6b2057fd9fdb6aa57
DaLizardWizard <75072026+DaLizardWizard@users.noreply.github.com>
2022-01-13 20:27:42 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ class ConvClassifier(nn.Module):
 st.write('# MNIST Digit Recognition')
 st.write('## Using a CNN `PyTorch` model')
 
-Network = torch.load('model.pth')
+Network = torch.load('shoop.pth')
 
 
 st.write('### Draw a digit in 0-9 in the box below')
